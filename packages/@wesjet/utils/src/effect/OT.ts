1b0d7adb4f64fb32af84324bbd353c4d48d3ae54
Wesbitty Inc <wesbitty@outlook.com>
2023-01-25 07:46:30 +0000

    Format: Prettier

--- packages/@wesjet/utils/src/effect/OT.ts
+++ packages/@wesjet/utils/src/effect/OT.ts
@@ -1,16 +1,18 @@
-import { Effect as T, Managed as M, Option as O, pipe } from '@effect-ts/core'
-import { pretty } from '@effect-ts/core/Effect/Cause/Pretty'
-import * as S from '@effect-ts/core/Effect/Experimental/Stream'
-import type { Has } from '@effect-ts/core/Has'
-import * as OT from '@effect-ts/otel'
-import * as OTApi from '@opentelemetry/api'
+import { Effect as T, Managed as M, Option as O, pipe } from "@effect-ts/core";
+import { pretty } from "@effect-ts/core/Effect/Cause/Pretty";
+import * as S from "@effect-ts/core/Effect/Experimental/Stream";
+import type { Has } from "@effect-ts/core/Has";
+import * as OT from "@effect-ts/otel";
+import * as OTApi from "@opentelemetry/api";
 
-export * from '@effect-ts/otel'
-export type HasSpan = Has<OT.Span>
+export * from "@effect-ts/otel";
+export type HasSpan = Has<OT.Span>;
 
 export const withStreamSpan =
   (name: string, options?: OTApi.SpanOptions, ctx?: OTApi.Context) =>
-  <R, E, A>(stream: S.Stream<R & HasSpan, E, A>): S.Stream<R & OT.HasTracer, E, A> =>
+  <R, E, A>(
+    stream: S.Stream<R & HasSpan, E, A>
+  ): S.Stream<R & OT.HasTracer, E, A> =>
     pipe(
       // NOTE we're using this weird `S.access` + `S.chain` here since `T.access` seems to be buggy
       // TODO fix this with Effect 2 🤠
@@ -22,38 +24,47 @@ export const withStreamSpan =
               M.makeExit_(
                 pipe(
                   T.succeedWith(() => {
-                    const { tracer } = OT.Tracer.read(r)
+                    const { tracer } = OT.Tracer.read(r);
 
-                    const maybeSpan = OT.Span.readOption(r)
+                    const maybeSpan = OT.Span.readOption(r);
                     if (ctx) {
-                      return tracer.startSpan(name, options, ctx)
+                      return tracer.startSpan(name, options, ctx);
                     }
                     if (options?.root !== true && O.isSome(maybeSpan)) {
-                      const ctx = OTApi.trace.setSpan(OTApi.context.active(), maybeSpan.value.span)
-                      return tracer.startSpan(name, options, ctx)
+                      const ctx = OTApi.trace.setSpan(
+                        OTApi.context.active(),
+                        maybeSpan.value.span
+                      );
+                      return tracer.startSpan(name, options, ctx);
                     }
-                    return tracer.startSpan(name, { ...options, root: true })
-                  }),
+                    return tracer.startSpan(name, { ...options, root: true });
+                  })
                 ),
 
                 (s, e) =>
                   T.succeedWith(() => {
-                    if (e._tag === 'Failure') {
-                      s.setAttribute('error.type', 'Fiber Failure')
-                      s.setAttribute('error.message', 'An Effect Has A Failure')
-                      s.setAttribute('error.stack', pretty(e.cause))
-                      s.setStatus({ code: OTApi.SpanStatusCode.ERROR })
+                    if (e._tag === "Failure") {
+                      s.setAttribute("error.type", "Fiber Failure");
+                      s.setAttribute(
+                        "error.message",
+                        "An Effect Has A Failure"
+                      );
+                      s.setAttribute("error.stack", pretty(e.cause));
+                      s.setStatus({ code: OTApi.SpanStatusCode.ERROR });
                     } else {
-                      s.setStatus({ code: OTApi.SpanStatusCode.OK })
+                      s.setStatus({ code: OTApi.SpanStatusCode.OK });
                     }
-                    s.end()
-                  }),
-              ),
-            )
+                    s.end();
+                  })
+              )
+            );
 
-            return pipe(stream, S.provideAll({ ...r, ...OT.Span.has(new OT.SpanImpl(span)) }))
+            return pipe(
+              stream,
+              S.provideAll({ ...r, ...OT.Span.has(new OT.SpanImpl(span)) })
+            );
           }),
-          S.unwrapManaged,
-        ),
-      ),
-    )
+          S.unwrapManaged
+        )
+      )
+    );
--- packages/wesjet-simple-import-hooks/src/lib/index.ts
+++ packages/wesjet-simple-import-hooks/src/lib/index.ts
@@ -1,5 +1,7 @@
-import type * as StackbitTypes from '@stackbit/types'
+import type * as StackbitTypes from "@stackbit/types";
 
-export type Transform = (config: StackbitTypes.StackbitConfig) => StackbitTypes.StackbitConfig
+export type Transform = (
+  config: StackbitTypes.StackbitConfig
+) => StackbitTypes.StackbitConfig;
 
-export const defineTransform = (transform: Transform): Transform => transform
+export const defineTransform = (transform: Transform): Transform => transform;
