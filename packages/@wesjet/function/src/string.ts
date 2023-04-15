c00541e87435b2f8d79d1fc3397a2ea229378eac
DimejiSR <iamsharaff@gmail.com>
2023-04-15 14:51:01 -0500

    Update string.ts
    Signed-off-by: DimejiSR <iamsharaff@gmail.com>

--- packages/@wesjet/function/src/string.ts
+++ packages/@wesjet/function/src/string.ts
@@ -1,2 +1,11 @@
+/**
+ * Copyright (c) Wesbitty, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @dimejiSR
+ */
+
 export const lowercaseFirstChar = (str: string) => str.charAt(0).toLowerCase() + str.slice(1)
 export const uppercaseFirstChar = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
