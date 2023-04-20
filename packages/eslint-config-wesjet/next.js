4b93dbc147b2807ba5e5bf6bda16622948ce3570
Dimeji SR <iamsharaff@gmail.com>
2023-04-20 11:56:55 +0000

    v31

--- packages/eslint-config-wesjet/next.js
+++ packages/eslint-config-wesjet/next.js
@@ -1,1 +1,27 @@
-module.exports = require('./rules/core-web-vital.js')
+module.exports = {
+  extends: ['wesjet/preset', '@next/next/recommended', 'turbo'],
+  // env
+  env: {
+    browser: true,
+    node: true,
+  },
+  // Parser
+  parserOptions: {
+    requireConfigFile: false,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+    babelOptions: {
+      presets: ['next/babel'],
+      caller: {
+        // Eslint supports top level await when a parser for it is included. We enable the parser by default for Babel.
+        supportsTopLevelAwait: true,
+      },
+    },
+  },
+  rules: {
+    '@next/next/no-html-link-for-pages': 'off',
+    '@next/next/no-page-custom-font': 'off',
+    '@next/next/no-img-element': 'off',
+    'turbo/no-undeclared-env-vars': 'off',
+  },
+}
